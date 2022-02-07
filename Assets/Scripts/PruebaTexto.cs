9f9eb4e7eea161d67b789c50ec64510d45218d83
DESKTOP-195V6S2\USUARIO <dafne.aml7@gmail.com>
2022-02-06 21:55:57 -0600

    Prueba de txt

---
+++ Assets/Scripts/PruebaTexto.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PruebaTexto : MonoBehaviour
+{
+    public TextMeshProUGUI textoEditable;
+    public Color colorTexto;
+
+    void Start()
+    {
+        textoEditable.text = "Hola c:";
+        textoEditable.color = colorTexto;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
